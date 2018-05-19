2e6f084474b2fcaaa67387cdb732edb884316361
KarenMacario24 <chelymac21@gmail.com>
2018-05-19 14:24:01 -0600

    holaxd

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>PHP</title>
+	</head>
+	<body>
+		<p>
+			<?php
+			//Declaracion de variables
+			$Pcantidad=17;
+			$Scantidad=3;
+			$Total=$Pcantidad+$Scantidad;
+			if($Total>=18){
+				echo "mayor a 18";
+			}else {
+				echo "menor a 18";
+			}
+
+			
+	
+		?>
+		</p>
+	</body>
+</html>
